d8021b644960a3484b087e5ee2f1192b69d4e3a6
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-01-24 12:04:59 -0300

    error treatment with AppError and database connection

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require('../utils/AppError')
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body
+
+    // if (!name) {
+    //   throw new AppError('O nome é obrigatório!')
+    // }
 
     return response.json({ name, email, password }, 201)
   }
