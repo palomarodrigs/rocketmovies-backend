879f31f7b9474da523efe45e0929afc473e0d894
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2023-01-24 22:20:55 -0300

    update user done

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs')
+const { hash, compare } = require('bcryptjs')
 const AppError = require('../utils/AppError')
 const sqliteConnection = require('../database/sqlite')
 class UsersController {
@@ -19,6 +19,53 @@ class UsersController {
       email,
       hashedPassword
     ])
+
+    return response.json()
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if (!user) {
+      throw new AppError('User not found')
+    }
+
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('This email is already in use.')
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (password && !old_password) {
+      throw new AppError('You need to enter the old password to set the new password!')
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError('The old password does not match.')
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(
+      ` UPDATE users SET
+        name = ?,
+        email = ?,
+        password = ?,
+        updated_at = DATETIME('now')
+        WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    )
 
     return response.json()
   }
