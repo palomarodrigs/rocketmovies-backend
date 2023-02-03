de732c316134cfbcaae0debc380b9c6b5a249f36
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2023-02-03 16:17:38 -0300

    upload image profile done

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 
 const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError')
+const uploadConfig = require('./configs/upload')
 
 const routes = require('./routes')
 
@@ -10,6 +11,8 @@ migrationsRun()
 
 const app = express()
 app.use(express.json())
+
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 
 app.use(routes)
 
