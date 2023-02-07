a49a3fa05c185a82902584c71657bf2b6dabac69
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2023-02-06 23:37:18 -0300

    refatorations

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -59,12 +59,10 @@ class NotesController {
         .whereLike('notes.title', `%${title}%`)
         .whereIn('name', filterTags)
         .innerJoin('notes', 'notes.id', 'tags.note_id')
+        .groupBy('notes.id')
         .orderBy('notes.title')
     } else {
-      notes = await knex('notes')
-        .where({ user_id })
-        .whereLike('title', `%${title}%`)
-        .orderBy('title')
+      notes = await knex('notes').where({ user_id }).whereLike('title', `%${title}%`).orderBy('title')
     }
 
     const userTags = await knex('tags').where({ user_id })
